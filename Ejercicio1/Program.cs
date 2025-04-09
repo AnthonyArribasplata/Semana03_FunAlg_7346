b9a0ea2412213c9a6ee172b170869880bf0b3248
Anthony Arribasplata <anthony@upn.pe>
2025-04-08 19:07:02 -0500

    Termine mi primer Algoritmo

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -10,6 +10,21 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            int num1;
+            int num2;
+            int suma;
+            //Console.ReadKey();
+
+            Console.Write("Ingrese numero 1: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            //Console.ReadLine es igual al LEER de PSEINT
+            Console.Write("Ingrese numero 2: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            suma = num1 + num2;
+
+            Console.WriteLine("La suma es: "+suma);
         }
     }
 }
